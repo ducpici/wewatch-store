32f6d1dd64888dca488b7acaccb9f2590b321b18
phamduc <phamcongducx9@gmail.com>
2025-08-30 08:52:06 +0700

    update

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -14,80 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 configViewEngine(app);
 checkConnection();
 
-// app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 app.use(express.static("./public/"));
-
-const allowedOrigins = [
-    "https://admin.wewatch.com:5173",
-    "https://shop.wewatch.com:5174",
-];
-
-// app.use(
-//     cors({
-//         origin: function (origin, callback) {
-//             if (!origin || allowedOrigins.includes(origin)) {
-//                 callback(null, true);
-//             } else {
-//                 console.warn("Blocked CORS origin:", origin);
-//                 callback(new Error("Not allowed by CORS"));
-//             }
-//         },
-//         credentials: true, // Bắt buộc để gửi cookie
-//     })
-// );
-
-// Session config
-// const sessionShop = session({
-//     name: "shop.sid",
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         httpOnly: true,
-//         secure: true, // Bắt buộc khi dùng HTTPS
-//         sameSite: "none", // Cần cho cross-site
-//         domain: ".wewatch.com", // Cho phép chia sẻ giữa subdomain
-//         maxAge: 24 * 60 * 60 * 1000, // 1 ngày
-//     },
-// });
-
-// const sessionAdmin = session({
-//     name: "admin.sid",
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         httpOnly: true,
-//         secure: true,
-//         sameSite: "none",
-//         domain: ".wewatch.com",
-//         maxAge: 24 * 60 * 60 * 1000,
-//     },
-// });
-
-// // Middleware chọn session phù hợp
-// app.use((req, res, next) => {
-//     const origin = req.headers.origin || req.hostname;
-
-//     if (
-//         origin?.includes("shop.wewatch.com") ||
-//         origin?.includes("localhost:5174")
-//     ) {
-//         sessionShop(req, res, next);
-//     } else if (
-//         origin?.includes("admin.wewatch.com") ||
-//         origin?.includes("localhost:5173")
-//     ) {
-//         sessionAdmin(req, res, next);
-//     } else {
-//         console.warn("Unknown origin for session:", origin);
-//         next();
-//     }
-// });
 
 app.use(
     cors({
-        origin: ["http://localhost:3001", "http://localhost:3002"],
+        origin: true,
         credentials: true,
     })
 );
